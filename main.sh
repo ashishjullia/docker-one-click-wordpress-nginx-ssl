7acb7332ef2967de38d65237d4f78f8e9d5cc43e
ashishjullia19 <ashishjullia19@gmail.com>
2020-08-26 00:53:15 +0000

    Restructured and renamed a few files.

--- main.sh
+++ main.sh
@@ -18,15 +18,13 @@ sudo sh get-docker.sh
 sudo curl -L "https://github.com/docker/compose/releases/download/1.26.2/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 
+# compose file without ssl-port and dhparam
+envsubst < "./docker-compose-templates/docker-compose-http.yml.template" > "docker-compose.yml"
+
 # Create services "nodeJS", "webserver (nginx)" and "certbot"
 sudo docker-compose up -d
 
-envsubst < "./nginx/default.conf.template" > "./nginx/default.conf"
-
-envsubst < "docker-compose.yml.template" > "docker-compose.yml"
-
-# # Create the containers
-sudo docker-compose up -d 
+envsubst < "./nginx/default-http.conf.template" > "./nginx/default.conf"
 
 sleep 10
 
@@ -35,7 +33,9 @@ sudo docker-compose stop webserver
 mkdir dhparam && \
 sudo openssl dhparam -out $PWD/dhparam/dhparam-2048.pem 2048
 
-envsubst < "default-ssl.conf.template" > "default.conf"
+envsubst < "./nginx/default-https.conf.template" > "default.conf"
+
+envsubst < "./docker-compose-templates/docker-compose-https.yml.template" > "docker-compose.yml"
 
 sudo docker-compose up -d --force-recreate --no-deps webserver
 
